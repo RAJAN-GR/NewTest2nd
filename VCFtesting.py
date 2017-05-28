d1d9bde51e5a13d1bb33e0387f4cbb07ce5df9f6
RAJAN-GR <rajanrajgor8@gmail.com>
2017-05-29 04:29:06 +0530

    Add
    Add this new file to the main repository.

---
+++ VCFtesting.py
@@ -0,0 +1,9 @@
+# This code for testing purpose of ".vcf " file.
+
+import vcf
+
+vcf_reader = vcf.Reader(open(r'D:\Software\Contact manager in smart phones/00001.vcf', 'r'))
+
+for record in vcf_reader:
+	print (record,sec)
+
